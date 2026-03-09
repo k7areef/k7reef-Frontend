67ef5b86df6f350037e1692e429025693bee678f
k7areef <kh3reef@gmail.com>
2026-03-09 19:53:21 +0200

    Update Skill Card

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -12,8 +12,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SkillCard({ skill = {} }) {
     return (
-        <div className="skill-card p-5 rounded-lg flex flex-col gap-3 items-center bg-grey transition-colors duration-300 ease-out border-2 border-transparent sm:hover:border-primary">
-            <div className="skill-image w-16 h-16 flex items-center justify-center">
+        <div className={`skill-card p-5 rounded-lg flex flex-col gap-3 items-center bg-grey transition-colors duration-300 ease-out border-2 ${skill.incoming ? "border-primary" : "border-transparent sm:hover:border-primary"} relative`}>
+            {/* Incoming */}
+            {
+                skill.incoming && <div className="incoming absolute py-1 px-2 text-xs left-2 top-2 bg-primary font-medium rounded-sm italic">
+                    Incomin
+                </div>
+            }
+            {/* Image */}
+            <div className={`skill-image w-16 h-16 flex items-center justify-center${skill.incoming ? " " + "opacity-50" : ""}`}>
                 {
                     String(skill.name).toLowerCase() === "github" ? (
                         <FontAwesomeIcon icon={faGithub} size="4x" />
@@ -28,7 +35,8 @@ function SkillCard({ skill = {} }) {
                     )
                 }
             </div>
-            <h3 aria-label={skill.name} className="skill-name font-semibold capitalize">{skill.name}</h3>
+            {/* Name */}
+            <h3 aria-label={skill.name} className={`skill-name font-semibold capitalize${skill.incoming ? " " + "opacity-50" : ""}`}>{skill.name}</h3>
         </div>
     )
 }

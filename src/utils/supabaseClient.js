0576a08d7f2a7f4537a3de818511b45856be733d
k7areef <kh3reef@gmail.com>
2026-03-14 14:00:35 +0200

    fix: add secret keys to env file

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tvdmztetyxgttwfxvfdq.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2ZG16dGV0eXhndHR3Znh2ZmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NDk5MzUsImV4cCI6MjA4OTAyNTkzNX0.ImVbEFuI7LZDzvU1Sf8sRYu3GtvIvfP_c78hh56IObw'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

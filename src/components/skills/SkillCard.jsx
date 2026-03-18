80fc1d0ffbaeb6b02d3daeb717ad5b4ca34df7d0
k7areef <kh3reef@gmail.com>
2026-03-18 02:02:01 +0200

    feat: add line through to skill name is archived

--- src/components/skills/SkillCard.jsx
+++ src/components/skills/SkillCard.jsx
@@ -18,7 +18,7 @@ function SkillCard({ skill = {} }) {
         <div className={`skill-card p-5 rounded-lg flex flex-col gap-3 items-center bg-grey transition-colors duration-300 ease-out border-2 ${normalStatus === "archived" ? "border-red-300 opacity-60" : normalStatus === "inprogress" ? "border-yellow-300 opacity-70" : normalStatus !== "active" ? "border-primary opacity-60" : "border-transparent sm:hover:border-primary"} relative`}>
             {/* Incoming */}
             {
-                normalStatus !== "active" && <div className={`badge absolute z-1 py-1 px-2 text-xs left-2 top-2 ${normalStatus === "archived" ? "bg-red-300" : normalStatus === "inprogress" ? "bg-yellow-300 text-dark" : "bg-primary"} font-medium rounded-sm italic`}>
+                normalStatus !== "active" && <div className={`badge absolute z-1 py-1 px-2 text-xs left-2 top-2 ${normalStatus === "archived" ? "bg-red-300 text-dark" : normalStatus === "inprogress" ? "bg-yellow-300 text-dark" : "bg-primary"} font-medium rounded-sm italic`}>
                     {skill.status}
                 </div>
             }
@@ -39,7 +39,7 @@ function SkillCard({ skill = {} }) {
                 }
             </div>
             {/* Name */}
-            <h3 aria-label={skill.name} className={`skill-name font-semibold capitalize`}>{skill.name}</h3>
+            <h3 aria-label={skill.name} className={`skill-name font-semibold capitalize ${normalStatus === "archived" ? "line-through text-red-300" : ""}`}>{skill.name}</h3>
         </div>
     )
 }
